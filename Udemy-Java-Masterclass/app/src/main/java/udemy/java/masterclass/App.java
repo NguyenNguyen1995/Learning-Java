db7161617876b34490ee87eac44a6d97bb624cd7
NguyenNguyen1995 <babyfacedeveloper@gmail.com>
2022-05-14 11:16:24 -0400

    collections methods

--- Udemy-Java-Masterclass/app/src/main/java/udemy/java/masterclass/App.java
+++ Udemy-Java-Masterclass/app/src/main/java/udemy/java/masterclass/App.java
@@ -3,20 +3,68 @@
  */
 package udemy.java.masterclass;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class App {
 
     public static void main(String[] args) {
         Theatre theatre = new Theatre("Olympian", 8, 12);
-        // theatre.getSeats();
-        if (theatre.reserveSeat("B13")) {
-            System.out.println("Please pay");
-        } else {
-            System.out.println("Sorry, seat is taken");
+        var seatCopy = new ArrayList<>(theatre.seats);
+        printList(seatCopy);
+
+        if (seatCopy.get(1) == theatre.seats.get(1)) {
+            System.out.println("seatCopy 1 equals to theatre.seats 1");
+            System.out.println("######################################################");
         }
-        // if(theatre.reserveSeat("H11")) {
-        // System.out.println("Please pay");
-        // } else {
-        // System.out.println("Sorry, seat is taken");
-        // }
+
+        if (seatCopy == theatre.seats) {
+            System.out.println("seatCopy equals to theatre.seats");
+            System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+        }
+
+        seatCopy.get(1).reserve();
+        if (theatre.reserveSeat("A02")) {
+            System.out.println("Please pay for A02");
+        } else {
+            System.out.println("Seat already reserved");
+        }
+
+        Collections.shuffle(seatCopy);
+        System.out.println("Printing seatCopy");
+        printList(seatCopy);
+        System.out.println("Printing theatre.seat");
+        printList(theatre.seats);
+
+        var minSeat = Collections.min(seatCopy);
+        var maxSeat = Collections.max(seatCopy);
+        System.out.println("Min seat number is " + minSeat.getSeatNumber());
+        System.out.println("Max seat number is " + maxSeat.getSeatNumber());
+
+        sortList(seatCopy);
+        System.out.println("Printing sorted seatCopy");
+        printList(seatCopy);
+
+        List<Theatre.Seat> newList = new ArrayList<>(theatre.seats.size());
+        Collections.copy(newList, theatre.seats);
+    }
+
+    public static void printList(List<Theatre.Seat> list) {
+        for (Theatre.Seat seat : list) {
+            System.out.print(" " + seat.getSeatNumber());
+        }
+        System.out.println();
+        System.out.println("===================================================================");
+    }
+
+    public static void sortList(List<? extends Theatre.Seat> list) {
+        for (int i = 0; i < list.size() - 1; i++) {
+            for (int j = i+1; j < list.size(); j++) {
+                if (list.get(i).compareTo(list.get(j)) > 0) {
+                    Collections.swap(list, i, j);
+                }
+            }
+        }
     }
 }
